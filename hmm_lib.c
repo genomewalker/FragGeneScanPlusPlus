c5ceb7e924eb4911aec889fb62df498c31286184
genomewalker <genomewalker@gmail.com>
2019-11-08 18:24:27 +0100

    back to tab

--- hmm_lib.c
+++ hmm_lib.c
@@ -1112,13 +1112,13 @@ void print_gene(Strand strand, int start_t, int end_t, int frame, char *output_b
     */
     /* Now fill the AA buffer with the translated proteins */
     if (multiple)
-        strcat(aa_buffer, "\v");
-        strcat(dna_buffer, "\v");
+        strcat(aa_buffer, "\t");
+        strcat(dna_buffer, "\t");
 
-    sprintf(temp_str_ptr, "%s_%d_%d_%c\t", sequence_head_short, start_t, end_t, strand_sign);
+    sprintf(temp_str_ptr, "%s_%d_%d_%c##", sequence_head_short, start_t, end_t, strand_sign);
     strcat(aa_buffer, temp_str_ptr);
     get_protein(dna_seq, dna_len, protein, strand);
-    sprintf(temp_str_ptr, "%s\t", protein);
+    sprintf(temp_str_ptr, "%s##", protein);
     strcat(aa_buffer, temp_str_ptr);
 
     /* Similar for the DNA buffer */
